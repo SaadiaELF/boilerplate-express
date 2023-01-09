3657c04cf9549aa0acb6ac15893c063a3dbf5f20
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-09 22:42:51 +0000

    added route

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 let express = require("express");
 let app = express();
 
-app.get("/", function () {
-  console.log("Hello Express");
+app.get("/", function (req, res) {
+  res.send("Hello Express");
 });
 
 module.exports = app;
